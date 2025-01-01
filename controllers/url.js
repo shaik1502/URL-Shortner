e72c6d9547312bb58dad74dd04abbc16ef59303d
Sameer Shaik <114245858+shaik1502@users.noreply.github.com>
2025-01-01 22:17:45 +0530

    short ids generated and redirecting urls completed

--- controllers/url.js
+++ controllers/url.js
@@ -1,5 +1,5 @@
 const Url = require("../models/url");
-const shortid = require('shortid');
+const shortid = require("shortid");
 
 async function handleGetURL(req, res) {
   const obj = await Url.find({});
@@ -7,32 +7,43 @@ async function handleGetURL(req, res) {
 }
 async function handlePostUrl(req, res) {
   const body = req.body;
-const shortidid = shortid.generate();
+  const shortidid = shortid.generate();
   const existingUrl = await Url.findOne({ original_url: body.url });
   if (existingUrl) {
     return res.status(409).json({ msg: "Url already in exists" });
   }
   const result = await Url.create({
     original_url: body.url,
-    short_id:shortidid,
-});
+    short_id: shortidid,
+  });
 
   res.status(201).json({ msg: "Success", id: result.id });
 }
-   
+
 async function handleGetURLbyID(req, res) {
   const obj = await Url.findById(req.params.id);
-  return res.status(200).json(obj);   
+  return res.status(200).json(obj);
 }
 
 async function handleDeleteUrlbyID(req, res) {
   await Url.findByIdAndDelete(req.params.id);
   return res.status(200).json({ msg: "Url Deleted Successfully" });
 }
+
+async function handleGetRedirectByUrl(req, res) {
+  const short1 = req.params.shortid;
+  console.log("Short URL received:", short1);
+  const entry = await Url.findOne({ short_id: short1 });
+  const originalUrl = entry.original_url;
+
+  console.log("Redirecting to:", originalUrl);
+  return res.redirect(originalUrl);
+}
+
 module.exports = {
   handleGetURL,
   handlePostUrl,
   handleGetURLbyID,
   handleDeleteUrlbyID,
+  handleGetRedirectByUrl,
 };
-     
--- routes/url.js
+++ routes/url.js
@@ -4,11 +4,16 @@ const {
   handlePostUrl,
   handleGetURLbyID,
   handleDeleteUrlbyID,
+  handleGetRedirectByUrl,
 } = require("../controllers/url");
 const router = express.Router();
 
 router.route("/").get(handleGetURL).post(handlePostUrl);
 
-router.route("/:id").get(handleGetURLbyID).delete(handleDeleteUrlbyID);
+router.route("/:shortid").get(handleGetRedirectByUrl)
+
+router.route("/:id").delete(handleDeleteUrlbyID); 
+// router.route("/:id").get(handleGetURLbyID); 
+
 
 module.exports = router;
